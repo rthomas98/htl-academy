cca1e0b75c80929e03b0970d801a8229f7fcefe6
Rob Thomas <robt84@gmail.com>
2023-03-28 16:11:41 -0500

    first commit

---
+++ page-marketing.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Template name: Marketing Page
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package BuddyBoss_Theme
+ */
+
+get_header('marketing');
+?>
+
+
+
+<?php
+get_footer('marketing');
